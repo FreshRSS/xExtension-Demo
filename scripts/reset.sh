031510ecf3e0a420237163fa5624373db0fe78e1
Marien Fressinaud <dev@marienfressinaud.fr>
2024-12-15 17:37:24 +0100

    Fix the reset script

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -5,16 +5,16 @@ DATA_DIR=$CURRENT_DIR/../../../data
 CLI_DIR=$CURRENT_DIR/../../../cli
 
 # Default user cannot be deleted by the CLI, so we delete the users manually
-rm -rf $DATA_DIR/users/*/*
+rm -rf $DATA_DIR/users/*
 $CLI_DIR/create-user.php --user demo --password 'demodemo' --language en
 $CLI_DIR/actualize-user.php --user demo
 
 $CLI_DIR/reconfigure.php \
-	--default_user demo \
-	--auth_type form \
+	--default-user demo \
+	--auth-type form \
 	--environment production \
-	--base_url https://demo.freshrss.org \
+	--base-url https://demo.freshrss.org \
 	--language en \
 	--title FreshRSS \
-	--allow_anonymous \
+	--allow-anonymous \
 	--db-type sqlite
